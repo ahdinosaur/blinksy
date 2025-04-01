89b5247fe30878ee394aa3450201077a58d89850
Michael Williams <hello@mikey.nz>
2025-04-01 15:10:09 +1300

    progress

--- src/layout.rs
+++ src/layout.rs
@@ -1,35 +1,64 @@
+use core::iter::once;
+
+use glam::Vec2;
+
 #[derive(Debug)]
 pub struct Layout1d;
 
 #[derive(Debug)]
-pub struct Point2d {
-    x: f32,
-    y: f32,
-}
-
-#[derive(Debug)]
 pub enum Shape2d {
-    Point(Point2d),
+    Point(Vec2),
     Line {
-        start: Point2d,
-        end: Point2d,
+        start: Vec2,
+        end: Vec2,
+        pixel_count: usize,
     },
     // Note: Expects leds to be wired along rows.
     Grid {
-        start: Point2d,
-        row_end: Point2d,
-        col_end: Point2d,
-        row_pixel_spacing: f32,
-        col_pixel_spacing: f32,
+        start: Vec2,
+        row_end: Vec2,
+        col_end: Vec2,
+        row_pixel_count: usize,
+        col_pixel_count: usize,
         /// Are rows of leds wired zig-zag or not
         serpentine: bool,
     },
     Arc {
-        center: Point2d,
+        center: Vec2,
         radius: f32,
         angle_in_radians: f32,
-        pixel_spacing: f32,
+        pixel_count: usize,
     },
+}
+
+impl Shape2d {
+    fn points(&self) -> impl Iterator<Item = &Vec2> {
+        match self {
+            Shape2d::Point(point) => once(point),
+            Shape2d::Line {
+                start,
+                end,
+                pixel_count,
+            } => {
+                let spacing = (start - end) / *pixel_count as f32;
+                (0..pixel_count).map(|index| start + index * spacing)
+            }
+            Shape2d::Grid {
+                start,
+                row_end,
+                col_end,
+                row_pixel_count,
+                col_pixel_count,
+                serpentine,
+            } => todo!(),
+            Shape2d::Arc {
+                center,
+                radius,
+                angle_in_radians,
+                pixel_count,
+            } => todo!(),
+        }
+    }
 }
 
 #[derive(Debug)]

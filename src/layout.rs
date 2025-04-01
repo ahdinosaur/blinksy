a5411b8de943e1f6cb55cd5de711409a6a768080
Michael Williams <hello@mikey.nz>
2025-04-01 13:20:49 +1300

    yeah

--- src/layout.rs
+++ src/layout.rs
@@ -2,7 +2,35 @@
 pub struct Layout1d;
 
 #[derive(Debug)]
-pub enum Shape2d {}
+pub struct Point2d {
+    x: f32,
+    y: f32,
+}
+
+#[derive(Debug)]
+pub enum Shape2d {
+    Point(Point2d),
+    Line {
+        start: Point2d,
+        end: Point2d,
+    },
+    // Note: Expects leds to be wired along rows.
+    Grid {
+        start: Point2d,
+        row_end: Point2d,
+        col_end: Point2d,
+        row_pixel_spacing: f32,
+        col_pixel_spacing: f32,
+        /// Are rows of leds wired zig-zag or not
+        serpentine: bool,
+    },
+    Arc {
+        center: Point2d,
+        radius: f32,
+        angle_in_radians: f32,
+        pixel_spacing: f32,
+    },
+}
 
 #[derive(Debug)]
 pub struct Layout2d<const NUM_SHAPES: usize>([Shape2d; NUM_SHAPES]);

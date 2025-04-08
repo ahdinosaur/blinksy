95821752140a9097dd08063cba3c02c505b15f17
Michael Williams <hello@mikey.nz>
2025-04-08 22:03:43 +1200

    oh wow

--- core/src/control.rs
+++ core/src/control.rs
@@ -7,27 +7,21 @@ use crate::{
     pattern::{Pattern1d, Pattern2d},
 };
 
-pub struct Control1d<Layout, Pattern, Driver>
-where
-    Layout: Layout1d,
-    Pattern: Pattern1d<Layout>,
-    Driver: LedDriver,
-{
+pub struct Dim1d;
+pub struct Dim2d;
+
+pub struct Control<Dim, Layout, Pattern, Driver> {
+    dim: PhantomData<Dim>,
     layout: PhantomData<Layout>,
     pattern: Pattern,
     driver: Driver,
     brightness: f32,
 }
 
-impl<Layout, Pattern, Driver> Control1d<Layout, Pattern, Driver>
-where
-    Layout: Layout1d,
-    Pattern: Pattern1d<Layout>,
-    Driver: LedDriver,
-    Driver::Color: FromColor<Pattern::Color>,
-{
+impl<Dim, Layout, Pattern, Driver> Control<Dim, Layout, Pattern, Driver> {
     pub fn new(pattern: Pattern, driver: Driver) -> Self {
         Self {
+            dim: PhantomData,
             layout: PhantomData,
             pattern,
             driver,
@@ -38,61 +32,45 @@ where
     pub fn set_brightness(&mut self, brightness: f32) {
         self.brightness = brightness;
     }
+}
 
+impl<Layout, Pattern, Driver> Control<Dim1d, Layout, Pattern, Driver>
+where
+    Layout: Layout1d,
+    Pattern: Pattern1d<Layout>,
+    Driver: LedDriver,
+    Driver::Color: FromColor<Pattern::Color>,
+{
     pub fn tick(&mut self, time_in_ms: u64) -> Result<(), Driver::Error> {
         let pixels = self.pattern.tick(time_in_ms);
         self.driver.write(pixels, self.brightness)
     }
 }
 
-pub struct Control2d<Layout, Pattern, Driver>
-where
-    Layout: Layout2d,
-    Pattern: Pattern2d<Layout>,
-    Driver: LedDriver,
-{
-    layout: PhantomData<Layout>,
-    pattern: Pattern,
-    driver: Driver,
-    brightness: f32,
-}
-
-impl<Layout, Pattern, Driver> Control2d<Layout, Pattern, Driver>
+impl<Layout, Pattern, Driver> Control<Dim2d, Layout, Pattern, Driver>
 where
     Layout: Layout2d,
     Pattern: Pattern2d<Layout>,
     Driver: LedDriver,
     Driver::Color: FromColor<Pattern::Color>,
 {
-    pub fn new(pattern: Pattern, driver: Driver) -> Self {
-        Self {
-            layout: PhantomData,
-            pattern,
-            driver,
-            brightness: 1.,
-        }
-    }
-
-    pub fn set_brightness(&mut self, brightness: f32) {
-        self.brightness = brightness;
-    }
-
     pub fn tick(&mut self, time_in_ms: u64) -> Result<(), Driver::Error> {
         let pixels = self.pattern.tick(time_in_ms);
         self.driver.write(pixels, self.brightness)
     }
 }
 
-pub struct ControlBuilder<Layout, Pattern, Driver> {
-    pub layout: PhantomData<Layout>,
-    pub pattern: Pattern,
-    pub driver: Driver,
+pub struct ControlBuilder<Dim, Layout, Pattern, Driver> {
+    dim: PhantomData<Dim>,
+    layout: PhantomData<Layout>,
+    pattern: Pattern,
+    driver: Driver,
 }
 
-#[allow(clippy::new_without_default)]
-impl ControlBuilder<(), (), ()> {
-    pub fn new() -> Self {
+impl ControlBuilder<(), (), (), ()> {
+    pub fn new_1d() -> ControlBuilder<Dim1d, (), (), ()> {
         ControlBuilder {
+            dim: PhantomData,
             layout: PhantomData,
             pattern: (),
             driver: (),
@@ -100,9 +78,24 @@ impl ControlBuilder<(), (), ()> {
     }
 }
 
-impl<Pattern, Driver> ControlBuilder<(), Pattern, Driver> {
-    pub fn with_layout<Layout>(self) -> ControlBuilder<Layout, Pattern, Driver> {
+impl ControlBuilder<(), (), (), ()> {
+    pub fn new_2d() -> ControlBuilder<Dim2d, (), (), ()> {
         ControlBuilder {
+            dim: PhantomData,
+            layout: PhantomData,
+            pattern: (),
+            driver: (),
+        }
+    }
+}
+
+impl<Pattern, Driver> ControlBuilder<Dim1d, (), Pattern, Driver> {
+    pub fn with_layout<Layout>(self) -> ControlBuilder<Dim1d, Layout, Pattern, Driver>
+    where
+        Layout: Layout1d,
+    {
+        ControlBuilder {
+            dim: PhantomData,
             layout: PhantomData,
             pattern: self.pattern,
             driver: self.driver,
@@ -110,19 +103,34 @@ impl<Pattern, Driver> ControlBuilder<(), Pattern, Driver> {
     }
 }
 
-impl<Layout, Driver> ControlBuilder<Layout, (), Driver>
+impl<Pattern, Driver> ControlBuilder<Dim2d, (), Pattern, Driver> {
+    pub fn with_layout<Layout>(self) -> ControlBuilder<Dim2d, Layout, Pattern, Driver>
+    where
+        Layout: Layout2d,
+    {
+        ControlBuilder {
+            dim: PhantomData,
+            layout: PhantomData,
+            pattern: self.pattern,
+            driver: self.driver,
+        }
+    }
+}
+
+impl<Layout, Driver> ControlBuilder<Dim1d, Layout, (), Driver>
 where
     Layout: Layout1d,
 {
-    pub fn with_pattern1d<Pattern>(
+    pub fn with_pattern<Pattern>(
         self,
         params: Pattern::Params,
-    ) -> ControlBuilder<Layout, Pattern, Driver>
+    ) -> ControlBuilder<Dim1d, Layout, Pattern, Driver>
     where
         Pattern: Pattern1d<Layout>,
     {
         let pattern = Pattern::new(params);
         ControlBuilder {
+            dim: self.dim,
             layout: self.layout,
             pattern,
             driver: self.driver,
@@ -130,19 +138,20 @@ where
     }
 }
 
-impl<Layout, Driver> ControlBuilder<Layout, (), Driver>
+impl<Layout, Driver> ControlBuilder<Dim2d, Layout, (), Driver>
 where
     Layout: Layout2d,
 {
-    pub fn with_pattern2d<Pattern>(
+    pub fn with_pattern<Pattern>(
         self,
         params: Pattern::Params,
-    ) -> ControlBuilder<Layout, Pattern, Driver>
+    ) -> ControlBuilder<Dim2d, Layout, Pattern, Driver>
     where
         Pattern: Pattern2d<Layout>,
     {
         let pattern = Pattern::new(params);
         ControlBuilder {
+            dim: self.dim,
             layout: self.layout,
             pattern,
             driver: self.driver,
@@ -150,12 +159,13 @@ where
     }
 }
 
-impl<Layout, Pattern> ControlBuilder<Layout, Pattern, ()> {
-    pub fn with_driver<Driver>(self, driver: Driver) -> ControlBuilder<Layout, Pattern, Driver>
+impl<Dim, Layout, Pattern> ControlBuilder<Dim, Layout, Pattern, ()> {
+    pub fn with_driver<Driver>(self, driver: Driver) -> ControlBuilder<Dim, Layout, Pattern, Driver>
     where
         Driver: LedDriver,
     {
         ControlBuilder {
+            dim: self.dim,
             layout: self.layout,
             pattern: self.pattern,
             driver,
@@ -163,26 +173,26 @@ impl<Layout, Pattern> ControlBuilder<Layout, Pattern, ()> {
     }
 }
 
-impl<Layout, Pattern, Driver> ControlBuilder<Layout, Pattern, Driver>
+impl<Layout, Pattern, Driver> ControlBuilder<Dim1d, Layout, Pattern, Driver>
 where
     Layout: Layout1d,
     Pattern: Pattern1d<Layout>,
     Driver: LedDriver,
     Driver::Color: FromColor<Pattern::Color>,
 {
-    pub fn build1d(self) -> Control1d<Layout, Pattern, Driver> {
-        Control1d::new(self.pattern, self.driver)
+    pub fn build(self) -> Control<Dim1d, Layout, Pattern, Driver> {
+        Control::new(self.pattern, self.driver)
     }
 }
 
-impl<Layout, Pattern, Driver> ControlBuilder<Layout, Pattern, Driver>
+impl<Layout, Pattern, Driver> ControlBuilder<Dim2d, Layout, Pattern, Driver>
 where
     Layout: Layout2d,
     Pattern: Pattern2d<Layout>,
     Driver: LedDriver,
     Driver::Color: FromColor<Pattern::Color>,
 {
-    pub fn build2d(self) -> Control2d<Layout, Pattern, Driver> {
-        Control2d::new(self.pattern, self.driver)
+    pub fn build(self) -> Control<Dim2d, Layout, Pattern, Driver> {
+        Control::new(self.pattern, self.driver)
     }
 }

db27c367615e7cede9f34bc7bf44b7775bbfe840
Michael Williams <hello@mikey.nz>
2025-05-11 23:41:24 +1200

    okay

--- blinksy/src/color/hue.rs
+++ blinksy/src/color/hue.rs
@@ -1,3 +1,7 @@
+use core::marker::PhantomData;
+
+use super::{LinearRgb, LinearRgbw, OutputColor};
+
 pub struct Hue<M: HueMap = HueRainbow> {
     map: PhantomData<M>,
     inner: f32,
@@ -9,14 +13,19 @@ pub struct Hsi<M: HueMap = HueRainbow> {
     intensity: f32,
 }
 
-impl<M: HueMap> Hsi<M> {
-    pub fn to_rgb() -> Rgb {}
-
-    pub fn to_rgbw() -> Rgbw {}
+impl<M: HueMap> OutputColor for Hsi<M> {
+    fn to_linear_rgb(self) -> LinearRgb {
+        todo!()
+    }
+    fn to_linear_rgbw(self) -> LinearRgbw {
+        todo!()
+    }
 }
 
 pub struct HueRainbow;
 
 pub struct HueSpectrum;
 
-pub trait HueMap {}
+pub trait HueMap {
+    // ???
+}

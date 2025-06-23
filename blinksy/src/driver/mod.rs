f9ce9c879f506dbfd86a77fa5e3e5866009a1fb1
Michael Williams <hello@mikey.nz>
2025-06-24 11:13:32 +1200

    Better this time, implement async clocked

--- blinksy/src/driver/mod.rs
+++ blinksy/src/driver/mod.rs
@@ -14,7 +14,7 @@ pub mod clockless;
 pub use clocked::*;
 pub use clockless::*;
 
-/// Core trait for all LED drivers.
+/// Core blocking trait for all LED drivers.
 ///
 /// This trait defines the common interface for sending color data to LED hardware,
 /// regardless of the specific protocol or chipset being used.
@@ -66,6 +66,72 @@ pub trait Driver {
     ///
     /// Result indicating success or an error
     fn write<I, C>(
+        &mut self,
+        pixels: I,
+        brightness: f32,
+        correction: ColorCorrection,
+    ) -> Result<(), Self::Error>
+    where
+        I: IntoIterator<Item = C>,
+        Self::Color: FromColor<C>;
+}
+
+#[cfg(feature = "async")]
+/// Core async trait for all LED drivers.
+///
+/// This trait defines the common interface for asynchronously sending color data to LED hardware,
+/// regardless of the specific protocol or chipset being used.
+///
+/// # Type Parameters
+///
+/// * `Error` - The error type that may be returned by the driver
+/// * `Color` - The color type accepted by the driver
+///
+/// # Example
+///
+/// ```rust
+/// use blinksy::{color::{ColorCorrection, FromColor, LinearSrgb}, driver::AsyncDriver};
+///
+/// struct MyAsyncDriver {
+///     // Implementation details
+/// }
+///
+/// impl AsyncDriver for MyAsyncDriver {
+///     type Error = ();
+///     type Color = LinearSrgb;
+///
+///     async fn write<I, C>(&mut self, pixels: I, brightness: f32, correction: ColorCorrection) -> Result<(), Self::Error>
+///     where
+///         I: IntoIterator<Item = C>,
+///         Self::Color: FromColor<C>,
+///     {
+///         // Async implementation of writing colors to the LED hardware
+///         Ok(())
+///     }
+/// }
+/// ```
+pub trait DriverAsync {
+    /// The error type that may be returned by the driver.
+    type Error;
+
+    /// The color type accepted by the driver.
+    type Color;
+
+    // See note about allow(async_fn_in_trait) in smart-leds-trait:
+    //   https://github.com/smart-leds-rs/smart-leds-trait/blob/faad5eba0f9c9aa80b1dd17e078e4644f11e7ee0/src/lib.rs#L59-L68
+    #[allow(async_fn_in_trait)]
+    /// Writes a sequence of colors to the LED hardware, asynchronously.
+    ///
+    /// # Arguments
+    ///
+    /// * `pixels` - Iterator over colors
+    /// * `brightness` - Global brightness scaling factor (0.0 to 1.0)
+    /// * `correction` - Color correction factors
+    ///
+    /// # Returns
+    ///
+    /// Future that resolves to a Result indicating success or an error
+    async fn write<I, C>(
         &mut self,
         pixels: I,
         brightness: f32,
